35e90ae54c71008873744adeb6bef2925ac8851f
Theodore Redlon <redlontheo@gmail.com>
2022-12-21 10:34:57 -0500

    Added a model for pizza posts

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    would_return = models.BooleanField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.name
